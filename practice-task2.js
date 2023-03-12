9658145fd6d4303c720a78112c0d829e52b95bef
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-12 17:31:19 +0600

    added p tag count dynamicly and it saved local storage

--- practice-task2.js
+++ practice-task2.js
@@ -1,6 +1,15 @@
 let count=0;
+const savedPTagCount=()=>{
+    const savedCount=localStorage.getItem('P Tag Count')
+    if (savedCount) {
+        count=savedCount;
+    }
+}
+
 const pTagCount=()=>{
+    savedPTagCount()
     count++;
     document.getElementById('p-count').innerText=count;
     localStorage.setItem('P Tag Count', count)
 }
+pTagCount()
