2c6b2db32ef832781231881939f5a9c3c39000a7
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-12 16:22:15 +0600

    added key and value in local staorage dynamicly

--- practice-task1.js
+++ practice-task1.js
@@ -1,3 +1,13 @@
 const clearLocalStorage=()=>{
     localStorage.clear()
 }
+
+const setToLocalStorage=()=>{
+    const getKeyInput=document.getElementById('key');
+    const getValueInput=document.getElementById('value');
+    const key=getKeyInput.value;
+    const value=getValueInput.value;
+    getKeyInput.value='';
+    getValueInput.value='';
+    localStorage.setItem(key,value)
+}
